23955c5a55888728f2002b331ab829b181d60b2c
Mohammad Ahmad <mohammad.ahmad@MACPRO215.local>
2024-08-01 14:13:35 +0530

    upload hellow module

---
+++ Gspann/Hellow/Controller/Index/Index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Gspann\Hellow\Controller\Index;
+
+
+/**
+ * Catalog index page controller.
+ */
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $resultPageFactory;
+    /**
+     * Index action
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect
+     */
+    public function __construct(
+      \Magento\Framework\App\Action\Context $context,
+      \Magento\Framework\View\Result\PageFactory $resultPageFactory
+      )
+    {
+      $this->resultPageFactory = $resultPageFactory;
+      parent::__construct($context);
+    }
+
+    public function execute()
+    {
+      echo "hellow Magento2";
+      return $this->$resultPageFactory->create();
+    }
+}
